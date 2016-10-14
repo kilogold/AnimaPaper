28406e6c20417b0d261a8d7358a03905ddcf64b2
IdolKeg <idolkeg@outlook.com>
2016-10-14 21:29:31 +0700

    Add project files.

---
+++ VideoDesk/Home.xaml.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoDesk
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : System.Windows.Controls.UserControl
+    {
+
+        bool onlyOne;
+        bool winSeven;
+        public static List<String> FileLoadList;
+        public static List<int> FileLoadListSort;
+        public static List<Uri> FileLoadListUri;
+        public Home()
+        {
+            InitializeComponent();
+            PlayButton.IsEnabled = false;
+            winSeven = false;
+            FileLoadList = new List<String>();
+            FileLoadListSort = new List<int>();
+            FileLoadListUri = new List<Uri>();
+            onlyOne = true;
+            Each.Visibility = Visibility.Hidden;
+            if (MainWindow.ScreenList.Count > 1)
+            {
+                Each.Visibility = Visibility.Visible;
+
+                onlyOne = false;
+                for (int i = 0; i < MainWindow.ButtonList.Count; i++)
+                {
+                    MainWindow.ButtonList[i].Click += new RoutedEventHandler(DynaButton_Click);
+                    FileLoadList.Add("");
+                   // FileLoadListUri.Add(new Uri(""));
+
+                    ms.Children.Add(MainWindow.ButtonList[i]);
+                }
+            }
+            if (onlyOne)
+                Multiple.IsEnabled = false;
+            else
+                Multiple.IsEnabled = true;
+        }
+
+        private void DynaButton_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            // Set filter for file extension and default file extension 
+            fileDialog.Filter = "All files (*.*)|*.*";
+
+            int t = Convert.ToInt32(Tag);
+            Console.WriteLine("Value = " + t);
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    FileLoadList.Add(fileDialog.FileName);
+                    FileLoadListUri.Add(new Uri(fileDialog.FileName));
+                    PlayButton.IsEnabled = true;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    FileSelected.Text = "nothing";
+                    PlayButton.IsEnabled = false;
+
+                    break;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            // Set filter for file extension and default file extension 
+            fileDialog.Filter = "All files (*.*)|*.*";
+
+
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    MainWindow.file = fileDialog.FileName;
+                    MainWindow.fileMedia = new Uri(fileDialog.FileName);
+                    FileSelected.Text = MainWindow.file;
+                    PlayButton.IsEnabled = true;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    FileSelected.Text = "nothing";
+                    PlayButton.IsEnabled = false;
+
+                    break;
+            }
+        }
+
+
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.findWorker();
+
+            if (winSeven)
+            {
+                Window sev = new Window();
+                IntPtr windowSev = new WindowInteropHelper(sev).Handle;
+
+                W32.SetParent(windowSev, MainWindow.workerw);
+
+            }
+            for (int i = 0; i < MainWindow.windowList.Count; i++)
+            {
+                MainWindow.windowList[i] = new Window();
+
+                MainWindow.windowList[i].WindowStyle = WindowStyle.None;
+                MainWindow.windowList[i].AllowsTransparency = true;
+
+                MainWindow.windowList[i].Top = MainWindow.ScreenList[i].Top;
+                MainWindow.windowList[i].Left = MainWindow.ScreenList[i].Left;
+                MainWindow.windowList[i].Width = MainWindow.ScreenList[i].Width;
+                MainWindow.windowList[i].Height = MainWindow.ScreenList[i].Height;
+
+                MainWindow.windowList[i].Initialized += new EventHandler((s, ea) =>
+                {
+                    MainWindow.media = new MediaElement();
+                    Grid grid = new Grid();
+                    MainWindow.windowList[i].Content = grid;
+                    grid.Children.Add(MainWindow.media);
+
+                    MainWindow.media.Source = MainWindow.fileMedia;
+                    MainWindow.media.LoadedBehavior = MediaState.Manual;
+
+                    if (MainWindow.soundOrNot)
+                        MainWindow.media.IsMuted = false;
+                    else
+                        MainWindow.media.IsMuted = true;
+                    MainWindow.media.Volume = 0;
+                    MainWindow.media.IsEnabled = true;
+                    MainWindow.media.Visibility = Visibility.Visible;
+                    MainWindow.media.MediaEnded += new RoutedEventHandler(m_MediaEnded);
+
+                    MainWindow.media.Play();
+                    MainWindow.currentlyPlaying = true;
+                    MainWindow.media.Stretch = Stretch.Fill;
+                    IntPtr windowHandle = new WindowInteropHelper(MainWindow.windowList[i]).Handle;
+                    if (winSeven)
+                        W32.SetParent(windowHandle, MainWindow.workerwHidden);
+                    else
+                        W32.SetParent(windowHandle, MainWindow.workerw);
+
+                });
+                MainWindow.windowList[i].UpdateLayout();
+
+                MainWindow.windowList[i].Show();
+            }
+
+        }
+
+        public static void m_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            MainWindow.media.Position = new TimeSpan(0, 0, 1);
+            //MainWindow.media.Position = TimeSpan.FromSeconds(0);
+            MainWindow.media.Play();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainWindow.currentlyPlaying == true)
+            {
+                MainWindow.media.Stop();
+                MainWindow.media = null;
+                for (int i = 0; i < MainWindow.windowList.Count; i++)
+                {
+                    MainWindow.windowList[i].Close();
+                }
+                MainWindow.currentlyPlaying = false;
+            }
+        }
+
+        private void Sound_Checked(object sender, RoutedEventArgs e)
+        {
+            MainWindow.soundOrNot = true;
+
+            if (MainWindow.currentlyPlaying == true)
+                MainWindow.media.IsMuted = false;
+            Sound.IsChecked = true;
+            Volume.IsEnabled = true;
+        }
+
+        private void Sound_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MainWindow.soundOrNot = false;
+            if (MainWindow.currentlyPlaying == true)
+                MainWindow.media.IsMuted = true;
+            Sound.IsChecked = false;
+            Volume.IsEnabled = false;
+        }
+
+        private void Volume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (MainWindow.currentlyPlaying == true)
+                MainWindow.media.Volume = Volume.Value;
+        }
+
+        private void Multiple_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MonoScreen.IsEnabled = true;
+
+            for (int i = 0; i < MainWindow.ButtonList.Count; i++)
+            {
+                MainWindow.ButtonList[i].IsEnabled = false;
+            }
+        }
+
+        private void Multiple_Checked(object sender, RoutedEventArgs e)
+        {
+            MonoScreen.IsEnabled = false;
+            for (int i = 0; i < MainWindow.ButtonList.Count; i++)
+            {
+                MainWindow.ButtonList[i].IsEnabled = true;
+            }
+
+        }
+
+        private void Windows_Checked(object sender, RoutedEventArgs e)
+        {
+            winSeven = true;
+        }
+
+        private void Windows_Unchecked(object sender, RoutedEventArgs e)
+        {
+            winSeven = false;
+        }
+    }
+}
---
+++ VideoDesk/MainWindow.xaml.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Media.Animation;
+using FirstFloor.ModernUI.Windows.Controls;
+using System.ComponentModel;
+using System.Collections;
+using System.Diagnostics;
+
+namespace VideoDesk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ModernWindow
+    {
+        public static String file;
+        public static Uri fileMedia;
+        public static Window win2;
+        public static MediaElement media = null;
+
+        public static List<Window> windowList;
+        public static List<System.Drawing.Rectangle> ScreenList;
+        public static List<System.Windows.Controls.Button> ButtonList;
+
+
+        public static IntPtr workerw;
+        public static IntPtr workerwHidden;
+
+
+        public static MediaPlayer player; //for thumbnails
+
+        public static bool soundOrNot;
+        public static bool currentlyPlaying;
+        System.Windows.Forms.NotifyIcon ni;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            media = null;
+            soundOrNot = false;
+            currentlyPlaying = false;
+            player = new MediaPlayer { Volume = 0, ScrubbingEnabled = true };
+            //this.WindowStyle = WindowStyle.SingleBorderWindow;
+
+            ni = new System.Windows.Forms.NotifyIcon();
+            ni.Icon = new System.Drawing.Icon("./AnimePaper.ico");
+
+            ni.DoubleClick +=
+                delegate (object sender, EventArgs args)
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                };
+
+
+
+            windowList = new List<Window>();
+            ScreenList = new List<System.Drawing.Rectangle>();
+            ButtonList = new List<System.Windows.Controls.Button>();
+            for (int i = 0; i < Screen.AllScreens.Length; i++)
+            {
+                ScreenList.Add(Screen.AllScreens[i].WorkingArea);
+            }
+            for (int i = 0; i < MainWindow.ScreenList.Count; i++)
+            {
+                windowList.Add(new Window());
+            }
+
+            for (int i = 0; i < MainWindow.ScreenList.Count; i++)
+            {
+                ButtonList.Add(new System.Windows.Controls.Button());
+                ButtonList[i].Name = "Screen" + i.ToString();
+                ButtonList[i].Content = "Screen " + i.ToString();
+                
+            }
+        }
+
+        private void Anime_Paper_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                ni.BalloonTipTitle = "Anime Paper";
+                ni.BalloonTipText = "Anime Paper is minimized";
+                ni.Visible = true;
+                ni.ShowBalloonTip(500);
+                this.Hide();
+            }
+            else if (WindowState.Normal == this.WindowState)
+            {
+                ni.Visible = false;
+            }
+
+            base.OnStateChanged(e);
+        }
+
+        public void closeAll()
+        {
+            if (MainWindow.media != null)
+            {
+                MainWindow.media.Stop();
+                MainWindow.media = null;
+                for (int i = 0; i < MainWindow.windowList.Count; i++)
+                {
+                    MainWindow.windowList[i].Close();
+                }
+
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+
+            ni.Visible = false;
+
+            if (MainWindow.media != null)
+            {
+                MainWindow.media.Stop();
+                MainWindow.media = null;
+                for (int i = 0; i < MainWindow.windowList.Count; i++)
+                {
+                    MainWindow.windowList[i].Close();
+                }
+
+            }
+            closeAll();
+            base.OnClosing(e);
+        }
+        public static void findWorker()
+        {
+            // Fetch the Progman window
+            IntPtr progman = W32.FindWindow("Progman", null);
+
+            IntPtr result = IntPtr.Zero;
+
+            W32.SendMessageTimeout(progman,
+                                   0x052C,
+                                   new IntPtr(0),
+                                   IntPtr.Zero,
+                                   W32.SendMessageTimeoutFlags.SMTO_NORMAL,
+                                   1000,
+                                   out result);
+
+            workerw = IntPtr.Zero;
+
+            W32.EnumWindows(new W32.EnumWindowsProc((tophandle, topparamhandle) =>
+            {
+                IntPtr p = W32.FindWindowEx(tophandle,
+                                            IntPtr.Zero,
+                                            "SHELLDLL_DefView",
+                                            IntPtr.Zero);
+
+                if (p != IntPtr.Zero)
+                {
+                    // Gets the WorkerW Window after the current one.
+                    workerw = W32.FindWindowEx(IntPtr.Zero,
+                                               tophandle,
+                                               "WorkerW",
+                                               IntPtr.Zero);
+                    
+                }
+
+                
+                return true;
+            }), IntPtr.Zero);
+
+
+            W32.EnumWindows(new W32.EnumWindowsProc((tophandle, topparamhandle) =>
+            {
+                IntPtr p = W32.FindWindowEx(tophandle,
+                                            IntPtr.Zero,
+                                            "Progman",
+                                            IntPtr.Zero);
+
+                if (p != IntPtr.Zero)
+                {
+                    // Gets the WorkerW Window after the current one.
+                    workerwHidden = W32.FindWindowEx(IntPtr.Zero,
+                                               tophandle,
+                                               "WorkerW",
+                                               IntPtr.Zero);
+
+                }
+
+                return true;
+            }), IntPtr.Zero);
+
+
+        }
+
+    }
+}
